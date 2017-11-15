4ac4c8794d32c866fbb17fef21d0f5f1c8624357
Ktara <ktaraash@gmail.com>
2017-11-15 19:45:47 +0200

    add calc slider

--- Crazy/js/script.js
+++ Crazy/js/script.js
@@ -25,3 +25,70 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1200);
     });
 });
+(function(){
+
+    var $slides = $(".calculate-slide-block");
+    var $line = $(".calculate-progress-line");
+    var $btnPrev = $(".btn-cal-prev");
+    var $btnNext = $(".btn-cal-next");
+    var counter = 0;
+    var linePosition = 8.5;
+    var oldPosition = 8.5 + 16.6*($slides.length - 2);
+
+    function nextSlide() {
+      
+      $($slides[counter]).animate({opacity:0}, 600);
+
+      if(counter === $slides.length-2)  {
+      $btnNext.css("display" , "none");
+      linePosition = 100;
+      } else {
+        linePosition = linePosition + 16.6;
+      }
+
+      $line.animate({
+        width : linePosition + "%"
+      }, 600);
+
+      setTimeout(function(){
+        $($slides[counter-1]).css("display", "none");
+      }, 600)
+
+      counter = counter + 1;
+
+      if(counter > 0) $btnPrev.css("display", "inline-block");
+
+      $($slides[counter]).css("display", "block").animate({opacity:1}, 600);
+    };
+    function prevSlide() {
+
+      $($slides[counter]).animate({opacity:0}, 600);
+      if(counter < $slides.length) $btnNext.css("display" , "inline-block"); 
+
+      if(counter === $slides.length - 1) {
+        linePosition = oldPosition;
+      } else {
+        linePosition = linePosition - 16.6;        
+      }
+
+
+      
+      $line.animate({
+        width : linePosition + "%"
+      }, 600);
+
+      setTimeout(function(){
+        $($slides[counter+1]).css("display", "none");
+      }, 600)
+
+      counter = counter - 1;
+
+      if(counter === 0) $btnPrev.css("display", "none");
+      $($slides[counter]).css("display", "block").animate({opacity:1}, 600);
+    };
+    $btnNext.click(nextSlide);
+    $btnPrev.click(prevSlide);
+
+
+})();
+
