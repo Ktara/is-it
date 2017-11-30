24b07cfb7679a1af6079c7a1ac9689f75a5c3c03
Ktara <ktaraash@gmail.com>
2017-12-01 01:14:36 +0200

    add slider logo

--- Inkorgaz/js/script.js
+++ Inkorgaz/js/script.js
@@ -1,25 +1,9 @@
-// $(document).ready(function(){
-//   $('.slider').slick({
-//     setting-name: setting-value
-//   });
-// });
-// $('.slider').slick({
-//   infinite: true,
-//   slidesToShow: 4,
-//   slidesToScroll: 4
-//   arrows: true,
-//   prevArrow: '<div class="prevarrow"></div>',
-//   nextArrow: '<div class="nextarrow"></div>',
-//   fade: true,
-//   infinite: true,
-// });
-// $('.slider-nav').slick({
-//   slidesToShow: 4,
-//   slidesToScroll: 4,
-//   asNavFor: '.slider',
-//   dots: false,
-//   centerMode: false,
-//   infinite: true,
-//   focusOnSelect: true
-// });
+$('.slider-logo').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: true,
+  prevArrow: '<div class="prevarrow"></div>',
+  nextArrow: '<div class="nextarrow"></div>'
+});
 	
