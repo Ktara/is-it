2d067dd16186b6174fac0c040985481a28ed1d73
Ktara <ktaraash@gmail.com>
2017-12-01 15:39:42 +0200

    add mobile version

--- Inkorgaz/js/script.js
+++ Inkorgaz/js/script.js
@@ -1,9 +1,25 @@
 $('.slider-logo').slick({
   infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToShow: 3,
+  slidesToScroll: 1,
   arrows: true,
   prevArrow: '<div class="prevarrow"></div>',
   nextArrow: '<div class="nextarrow"></div>'
 });
+
+console.log("rere");
+(function(){
+    $(".popup-button").click(function(e){
+      console.log("rere");
+        $(".customer-about-popup").addClass("popup-active");
+
+    });
+    $(".popup-close").click(function(e){
+       $(".customer-about-popup").removeClass("popup-active");
+    })
+     $(".customer-about-popup").click(function(e){
+      if(this != e.target) return;
+        $(".customer-about-popup").removeClass("popup-active");
+     })
+})();
 	
