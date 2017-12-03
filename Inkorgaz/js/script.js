f668779702fdd14890df82f938530814c5196eee
Ktara <ktaraash@gmail.com>
2017-12-03 15:42:28 +0200

    add some thing

--- Inkorgaz/js/script.js
+++ Inkorgaz/js/script.js
@@ -7,10 +7,9 @@ $('.slider-logo').slick({
   nextArrow: '<div class="nextarrow"></div>'
 });
 
-console.log("rere");
+
 (function(){
-    $(".popup-button").click(function(e){
-      console.log("rere");
+    $(".popup-button").click(function(e){     
         $(".customer-about-popup").addClass("popup-active");
 
     });
@@ -21,5 +20,42 @@ console.log("rere");
       if(this != e.target) return;
         $(".customer-about-popup").removeClass("popup-active");
      })
+})();
+(function(){
+   $('.show_search').click(function(e){
+      $('.show_search').css("display" , "none");
+      $('.search_tr').css({
+        "width" : "150px",
+        "visibility" : "visible"
+      });
+    $(document).click(function(e){
+      if(e.target === $('.search_tr')[0] || e.target ===  $('.show_search')[0]) {
+        return;
+      }
+      else{
+        $('.show_search').css("display" , "inline-block");
+        $('.search_tr').css({
+          "width" : "0",
+          "visibility" : "hidden"
+      });
+      };
+
+    })  
+   });
+})();
+(function(){
+  var status = false;
+  $("#nav_wrapper").click(function(){
+    if (parseInt($('body').css("width"), 10) > 768) return;
+        console.log('tets');
+    if(status === false) {
+       $(this).addClass("nav-active");
+       status = true;
+    } else {
+      $(this).removeClass("nav-active");
+      status = false;
+    }
+       
+  })
 })();
 	
