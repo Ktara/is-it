399f9c7d4a35c8b345fc5909f382c221c7d574e4
Ktara <ktaraash@gmail.com>
2017-12-04 14:11:04 +0200

    add text-justify

--- Inkorgaz/js/script.js
+++ Inkorgaz/js/script.js
@@ -78,3 +78,10 @@ $('.slider-logo-mobile').slick({
 {
   new ScrollFlow(); 
 } );
+
+  $(".link-btn").click(function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1000);
+});
