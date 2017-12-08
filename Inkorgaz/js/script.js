1deefc731c140c9bb4a1b63caf4a8fb41b43d190
Ktara <ktaraash@gmail.com>
2017-12-08 03:15:31 +0200

    add scroll

--- Inkorgaz/js/script.js
+++ Inkorgaz/js/script.js
@@ -85,8 +85,18 @@ $('.slider-logo-mobile').slick({
     $('body,html').animate({scrollTop: top}, 1000);
 });
 (function(){
+
+
+
+})(); 
+
+
+(function(){ 
+
+
   function animateText(textClass, time) {
     var $target = $(textClass);
+    $target.css("opacity", "1");
     var text = $target.text();
     $target.html('');
 
@@ -106,13 +116,76 @@ $('.slider-logo-mobile').slick({
     showSymbol();
 
   }
-  animateText(".animate_text", 2000);
-  animateText(".animate_text_1", 1000);
-  animateText(".animate_text_2", 1000);
-  animateText(".animate_text_3", 2000);
-  animateText(".animate_text_4", 700);
+  animateText(".animate_text", 2000);  
+  setTimeout(function(){
+      animateText(".animate_text_20", 2000);
+    }, 2000);
+
+  var $screen = screen.height/2;
+
+if($(".animate_text_1")[0] != undefined) {
+  var animtext1 = $(".animate_text_1");
+  var firstStage = animtext1.offset().top;
+  var firstStageStatus = false;
+}
+if($(".animate_text_2")[0] != undefined) {
+  var animtext2 = $(".animate_text_2");
+  var Stage2 = animtext2.offset().top;
+  var Stage2Status = false;
+}
+if($(".animate_text_3")[0] != undefined) {
+  var animtext3 = $(".animate_text_3");
+  var Stage3 = animtext3.offset().top;
+  var Stage3Status = false;
+}
+if($(".animate_text_4")[0] != undefined) {
+  var animtext4 = $(".animate_text_4");
+  var Stage4 = animtext4.offset().top;
+  var Stage4Status = false;
+}
+if($(".animate_text_5")[0] != undefined) {
+  var animtext5 = $(".animate_text_5");
+  var Stage5 = animtext5.offset().top;
+  var Stage5Status = false;
+}
+if($(".animate_text_6")[0] != undefined) {
+  var animtext6 = $(".animate_text_6");
+  var Stage6 = animtext6.offset().top;
+  var Stage6Status = false;
+}
+          window.onscroll = function() {
+                var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+                if($(".animate_text_1")[0] != undefined && scrolled > firstStage-$screen && firstStageStatus === false) {
+                    firstStageStatus = true;
+                    animateText(".animate_text_1", 1000);
+                }
+                if($(".animate_text_2")[0] != undefined && scrolled > Stage2-$screen && Stage2Status === false) {
+                    Stage2Status = true;
+                    animateText(".animate_text_2", 1000);
+                }
+                if($(".animate_text_3")[0] != undefined && scrolled > Stage3-$screen && Stage3Status === false) {
+                    Stage3Status = true;
+                    animateText(".animate_text_3", 2000);
+                }
+                if($(".animate_text_4")[0] != undefined && scrolled > Stage4-$screen && Stage4Status === false) {
+                    Stage4Status = true;
+                    animateText(".animate_text_4", 700);
+                } 
 
 
+                if($(".animate_text_5")[0] != undefined && scrolled > Stage5-$screen/2 && Stage5Status === false) {
+                    Stage5Status = true;
+                    animateText(".animate_text_5", 2000);
+                }
+                if($(".animate_text_6")[0] != undefined && scrolled > Stage6-$screen && Stage6Status === false) {
+                    Stage6Status = true;
+                    animateText(".animate_text_6", 2000);
+                }
 
-
-})();  
+            function getCoords(elem) {
+                var box = elem.getBoundingClientRect();
+                return box.top + pageYOffset;
+            }
+        };
+})(); 
