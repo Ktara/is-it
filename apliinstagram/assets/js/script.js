bb7aa3f39b4d50c2bb571e04040f9bca97670426
Ktara <ktaraash@gmail.com>
2018-03-10 10:49:00 +0200

    basket and nav

--- apliinstagram/assets/js/script.js
+++ apliinstagram/assets/js/script.js
@@ -1,26 +1,46 @@
-$('.wish_slider_wrapper').slick({
-	infinite: true,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	dots: true,
-	arrows: false,
-	autoplay: true,
-	autoplaySpeed: 2500,
-	fade: true,
-	zIndex: 100
+// $('.wish_slider_wrapper').slick({
+// 	infinite: true,
+// 	slidesToShow: 1,
+// 	slidesToScroll: 1,
+// 	dots: true,
+// 	arrows: false,
+// 	autoplay: true,
+// 	autoplaySpeed: 2500,
+// 	fade: true,
+// 	zIndex: 100
+// });
+
+
+// $('.product_slide').click(function(){
+// 	var slide_img = $(this).clone().css('opacity', '0');	
+// 	$('.product_slide_big').html(slide_img);
+// 	slide_img.animate({
+// 		opacity: 1
+// 	}, 200);
+// });
+
+
+// $( document ).ready(function(){
+// 	var heightimg = $('.product_slide_big').css('height');
+// 	$('.product_slider').css('height', heightimg);
+// })
+
+
+
+$('.nav_header_menu li').mouseover(function(){
+	$('.nav_list').velocity({'height': '45vh'}, 300);
+});
+
+$('.nav_list').mouseleave(function(){
+	$('.nav_list').velocity({'height': 0}, 150);
 });
 
 
-$('.product_slide').click(function(){
-	var slide_img = $(this).clone().css('opacity', '0');	
-	$('.product_slide_big').html(slide_img);
-	slide_img.animate({
-		opacity: 1
-	}, 200);
+$('.nav_basket').click(function(){
+	$('.basket_window').velocity({'right': 0}, 300);
 });
 
 
-$( document ).ready(function(){
-	var heightimg = $('.product_slide_big').css('height');
-	$('.product_slider').css('height', heightimg);
-})
+$('.basket_window_close').click(function(){
+	$('.basket_window').velocity({'right': '-420px'}, 300);
+});
