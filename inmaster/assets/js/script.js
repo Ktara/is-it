0ee0a79b92875ab77c8242edfeb0f062f4e14f3b
ktara <ktaraash@gmail.com>
2018-04-19 13:56:27 +0300

    add any section

--- inmaster/assets/js/script.js
+++ inmaster/assets/js/script.js
@@ -1,10 +1,22 @@
 $('.school_advantage_slider').slick({
-
     slidesToShow: 4,
     slidesToScroll: 4,
-
     dots: true,
     arrows: false,
-
     zIndex: 100
+});
+
+
+
+
+$('.school_gallery_slider_wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    nextArrow: '<div class="school_gallery_next_arrow"></div>',
+    prevArrow: '<div class="school_gallery_prev_arrow"></div>',
+    zIndex: 100,
+    adaptiveHeight: true,
+    infinite: true
 });
