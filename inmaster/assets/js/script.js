ebd318c02a3333447aba7239032cce79ca90b530
ktara <ktaraash@gmail.com>
2018-04-19 15:36:39 +0300

    fix main page

--- inmaster/assets/js/script.js
+++ inmaster/assets/js/script.js
@@ -1,4 +1,4 @@
-$('.school_advantage_slider').slick({
+$('.main_advantage_slider').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     dots: true,
@@ -9,13 +9,13 @@ $('.school_advantage_slider').slick({
 
 
 
-$('.school_gallery_slider_wrapper').slick({
+$('.main_gallery_slider_wrapper').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: false,
     arrows: true,
-    nextArrow: '<div class="school_gallery_next_arrow"></div>',
-    prevArrow: '<div class="school_gallery_prev_arrow"></div>',
+    nextArrow: '<div class="main_gallery_next_arrow"></div>',
+    prevArrow: '<div class="main_gallery_prev_arrow"></div>',
     zIndex: 100,
     adaptiveHeight: true,
     infinite: true
