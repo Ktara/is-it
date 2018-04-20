d5f722af206aca78e211812acedb0ec854f7cc47
ktara <ktaraash@gmail.com>
2018-04-20 16:19:21 +0300

    add about, learners, teach pages

--- inmaster/assets/js/script.js
+++ inmaster/assets/js/script.js
@@ -20,3 +20,33 @@ $('.main_gallery_slider_wrapper').slick({
     adaptiveHeight: true,
     infinite: true
 });
+
+
+
+
+$('.about_certificates_slider_wrapper').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    nextArrow: '<div class="about_certificates_next_arrow"></div>',
+    prevArrow: '<div class="about_certificates_prev_arrow"></div>',
+    zIndex: 100,
+    adaptiveHeight: true,
+    infinite: true
+});
+
+
+
+
+$('.about_reviews_slider_wrapper').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    nextArrow: '<div class="about_reviews_next_arrow"></div>',
+    prevArrow: '<div class="about_reviews_prev_arrow"></div>',
+    zIndex: 100,
+    adaptiveHeight: true,
+    infinite: true
+});
