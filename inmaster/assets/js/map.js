d16faaac82095511f35000277e51065a8242d9e9
ktara <ktaraash@gmail.com>
2018-04-27 09:52:25 +0300

    add nav header effect

--- inmaster/assets/js/map.js
+++ inmaster/assets/js/map.js
@@ -4,6 +4,26 @@ function initMap() {
         var map = new google.maps.Map(document.getElementById('contact_map'), {
             zoom: 18,
             center: uluru,
+            styles: [
+                {
+                    "featureType": "all",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "hue": "#007aff"
+                        },
+                        {
+                            "saturation": "0"
+                        },
+                        {
+                            "lightness": "0"
+                        },
+                        {
+                            "gamma": "1.00"
+                        }
+                    ]
+                }
+            ]
 
 
         });
--- inmaster/assets/js/script.js
+++ inmaster/assets/js/script.js
@@ -50,3 +50,35 @@ $('.about_reviews_slider_wrapper').slick({
     adaptiveHeight: true,
     infinite: true
 });
+
+
+
+var $dataTitles = $('.timetable_arrows_block');
+
+$('.timetable_slider_wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: '<div class="timetable_next_arrow"></div>',
+    prevArrow: '<div class="timetable_prev_arrow"></div>',
+    zIndex: 100,
+    fade: true,
+    adaptiveHeight: true,
+    dots: true,
+    customPaging : function(slider, i) {
+       // var length = slider.$slides.length;
+        return $dataTitles[i].dataset.title;
+    }
+});
+
+
+var heightFirstBlock = window.clientHeight || document.documentElement.clientHeight;
+
+window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > 1){
+        $('.nav_header').addClass('header_active');
+    } else {
+        $('.nav_header').removeClass('header_active');
+    }
+}
