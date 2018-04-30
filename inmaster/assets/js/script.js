86d2008a30f80e8c2e49a6ca298d644c9cfa80c2
ktara <ktaraash@gmail.com>
2018-04-30 12:54:04 +0300

    add mob version

--- inmaster/assets/js/script.js
+++ inmaster/assets/js/script.js
@@ -9,6 +9,18 @@ $('.main_advantage_slider').slick({
 
 
 
+$('.main_advantage_mob_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    zIndex: 100,
+    adaptiveHeight: true,
+});
+
+
+
+
 $('.main_gallery_slider_wrapper').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -39,8 +51,38 @@ $('.main_gallery_tablet_slider_wrapper').slick({
 
 
 
+$('.main_gallery_mob_slider_wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    nextArrow: '<div class="main_gallery_next_arrow"></div>',
+    prevArrow: '<div class="main_gallery_prev_arrow"></div>',
+    zIndex: 100,
+    adaptiveHeight: true,
+    infinite: true
+});
+
+
+
+
 $('.about_certificates_slider_wrapper').slick({
     slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    nextArrow: '<div class="about_certificates_next_arrow"></div>',
+    prevArrow: '<div class="about_certificates_prev_arrow"></div>',
+    zIndex: 100,
+    adaptiveHeight: true,
+    infinite: true
+});
+
+
+
+
+$('.about_certificates_mob_slider_wrapper').slick({
+    slidesToShow: 1,
     slidesToScroll: 1,
     dots: false,
     arrows: true,
@@ -71,6 +113,20 @@ $('.about_reviews_slider_wrapper').slick({
 
 $('.about_reviews_tablet_slider_wrapper').slick({
     slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    nextArrow: '<div class="about_reviews_next_arrow"></div>',
+    prevArrow: '<div class="about_reviews_prev_arrow"></div>',
+    zIndex: 100,
+    adaptiveHeight: true,
+    infinite: true,
+});
+
+
+
+$('.about_reviews_mob_slider_wrapper').slick({
+    slidesToShow: 1,
     slidesToScroll: 1,
     dots: false,
     arrows: true,
