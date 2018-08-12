283ba27f5fed9438e531b2bf4932349b46709d90
ktara <ktaraash@gmail.com>
2018-08-12 22:54:22 +0300

    hit_foto section

--- fitness/assets/js/script.js
+++ fitness/assets/js/script.js
@@ -1,15 +1,18 @@
-// var status = false;
-// $('.nav_header_menu').click(function(){
-// 	if (status === false) {
-// 		status = true;
-// 		$('.nav_header_menu').addClass('active_nav_wrapper');
-// 	} else {
-// 		status = false;
-// 		$('.nav_header_menu').removeClass('active_nav_wrapper');
-// 	}
-	
-
-// });
 $('.nav_header_menu_wrapper').click(function(){
 	$('.nav_header_menu_wrapper').toggleClass('active_nav_wrapper');
+});
+
+
+
+
+$('.hit_foto_slider_wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    nextArrow: '<div class="hit_foto_next_arrow"></div>',
+    prevArrow: '<div class="hit_foto_prew_arrow"></div>',
+    // zIndex: 100,
+    adaptiveHeight: true,
+    infinite: true
 });
