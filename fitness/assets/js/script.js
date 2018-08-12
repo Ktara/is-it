946cb850e38224a022789ddd29eb497d9932aa8c
ktara <ktaraash@gmail.com>
2018-08-12 11:12:03 +0300

    second section

---
+++ fitness/assets/js/script.js
@@ -0,0 +1,15 @@
+// var status = false;
+// $('.nav_header_menu').click(function(){
+// 	if (status === false) {
+// 		status = true;
+// 		$('.nav_header_menu').addClass('active_nav_wrapper');
+// 	} else {
+// 		status = false;
+// 		$('.nav_header_menu').removeClass('active_nav_wrapper');
+// 	}
+	
+
+// });
+$('.nav_header_menu_wrapper').click(function(){
+	$('.nav_header_menu_wrapper').toggleClass('active_nav_wrapper');
+});
