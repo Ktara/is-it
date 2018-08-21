83e37252c45df77eca50d8dab149e3dc2c4fb04e
ktara <ktaraash@gmail.com>
2018-08-21 16:00:58 +0300

    animate main page

--- fitness/assets/js/script.js
+++ fitness/assets/js/script.js
@@ -7,6 +7,34 @@ $(document).ready(function () {
             scrollTop: top
         }, 1200);
     });
+
+    $(".nav_header_menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href')
+            , top = $(id).offset().top - 0;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1200);
+    });
+
+    $(".main_btn").click(function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href')
+            , top = $(id).offset().top - 150;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1200);
+    });
+
+    $(".link_menu a").click(function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href')
+            , top = $(id).offset().top - 100;
+            console.log(id);
+        $('body,html').animate({
+            scrollTop: top
+        }, 1200);
+    });
 });
 
 
@@ -53,8 +81,45 @@ $('.popular_slider_wrapper').slick({
 
 
 
-
-
 $('.nav_header_log_out').click(function(){
-	$('.popup_log_out').css('display', 'block');
+	$('.popup_log_out').addClass('popup_log_out_active').velocity({
+		opacity: 1
+	}, 600);
 });
+
+$('.popup_log_out').click(function(event){	
+	if(event.target === this){		
+		$(this).velocity({
+			opacity: 0
+		}, 600);
+		setTimeout(function(){
+			$('.popup_log_out').removeClass('popup_log_out_active');
+		}, 600);
+	};
+});
+
+
+
+
+$('.nav_header_sign_in').click(function(){
+	$('.popup_sign_in').addClass('popup_sign_in_active').velocity({
+		opacity: 1
+	}, 600);
+});
+
+$('.popup_sign_in').click(function(event){	
+	if(event.target === this){		
+		$(this).velocity({
+			opacity: 0
+		}, 600);
+		setTimeout(function(){
+			$('.popup_sign_in').removeClass('popup_sign_in_active');
+		}, 600);
+	};
+});
+
+
+
+
+
+
