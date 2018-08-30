aeaaf0a7958e423f448121b265c6d4f2ba1aead2
ktara <ktaraash@gmail.com>
2018-08-30 13:23:19 +0300

    pravki 10

--- fitness/assets/js/script.js
+++ fitness/assets/js/script.js
@@ -116,6 +116,7 @@ $('.popular_slider_wrapper').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
+    appendDots: '<div class="white_block"></div>',
     arrows: true,
     nextArrow: '<div class="popular_next_arrow popular_arrow_animate"></div>',
     prevArrow: '<div class="popular_prew_arrow popular_arrow_animate"></div>',    
