4508d65b62347b22f6f80172e2937dc43eb55253
ktara <ktaraash@gmail.com>
2018-08-31 21:42:33 +0300

    fix color gym

--- fitness/assets/js/script.js
+++ fitness/assets/js/script.js
@@ -110,6 +110,20 @@ $('.hit_foto_slider_wrapper').slick({
 
 
 
+$('.foto_slider_wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    nextArrow: '<div class="hit_foto_next_arrow"></div>',
+    prevArrow: '<div class="hit_foto_prew_arrow"></div>',    
+    // adaptiveHeight: true,
+    infinite: true
+});
+
+
+
+
 
 
 $('.popular_slider_wrapper').slick({
